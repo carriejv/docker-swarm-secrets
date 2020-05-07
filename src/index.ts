e8f907709babf2bdf0ac8ed280f2adf6e865ef5d
Carrie J V <carrie@carriejv.com>
2020-05-06 18:01:03 -0700

    Typescript / 2.x rewrite.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {
+    DSSReader
+} from './reader/dss-reader';
+
+import {
+    DefaultInterpreters
+} from './interpreters/interpreters';
+
+export {
+    DSSReader,
+    DefaultInterpreters
+};
+
+export default DSSReader;
